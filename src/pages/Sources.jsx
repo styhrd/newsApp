fe8a4492ccc53385bff56f47ee1e11afdfd15131
styhrd <juanmiguelcaparas76@gmail.com>
2024-11-25 16:27:25 +0800

    Fetch Sources with redux

--- src/pages/Sources.jsx
+++ src/pages/Sources.jsx
@@ -1,12 +1,32 @@
-import React, { useEffect } from 'react'
-
-
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchSources } from '../store/sourcesSlice'
 const Sources = () => {
+    const dispatch = useDispatch();
+    const [category, setCategory] = useState('')
+    const [language, setLanguage] = useState('')
+    const [country, setCountry] = useState('')
+    const { sources } = useSelector((state) => state.sources)
+
+    useEffect(() => {
+        dispatch(fetchSources(category, language, country))
 
 
+    }, [category, language, country])
+
+    console.log(sources);
     return (
         <div>
-            Sources
+            <select onChange={(e) => setCategory(e.target.value)}>
+                <option value={'business'}>Business</option>
+                <option value={'entertainment'}>Entertainment</option>
+                <option value={'general'}>General</option>
+                <option value={'health'}>Health</option>
+                <option value={'science'}>Science</option>
+                <option value={'sports'}>Sports</option>
+                <option value={'technology'}>Technology</option>
+
+            </select>
         </div>
     )
 }
--- src/store/sourcesSlice.jsx
+++ src/store/sourcesSlice.jsx
@@ -5,14 +5,18 @@ import axios from "axios";
 const api_key = import.meta.env.VITE_API_KEY
 const url = 'https://newsapi.org/v2/top-headlines/sources'
 
-export const fetchSources = createAsyncThunk('sources/fetchSources', async () => {
+export const fetchSources = createAsyncThunk('sources/fetchSources', async (cat, lang, coun) => {
     const response = await axios.get(url, {
         params: {
-            apiKey: api_key
+            apiKey: api_key,
+            category: cat,
+            language: lang,
+            country: coun
+
         }
     })
 
-    return response.data.sources
+    return response.data.sources || []
 })
 
 const sourcesSlice = createSlice({
