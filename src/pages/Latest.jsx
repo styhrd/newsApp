eb3635ca3f02152e6367c80ce2a023829b90d045
styhrd <juanmiguelcaparas76@gmail.com>
2024-11-26 15:16:53 +0800

    Latest finished

--- src/pages/Latest.jsx
+++ src/pages/Latest.jsx
@@ -4,37 +4,75 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Latest = () => {
     const dispatch = useDispatch();
+    const { latestNews } = useSelector((state) => state.latest); // Correct state path
 
-    const [pagenum, setpagenum] = useState(1); // Set initial page to 1
-    const { latestNews } = useSelector((state) => state.latest); // Corrected state path
+    const [pagenum, setPagenum] = useState(1); // Initial page set to 1
+    const [query, setQuery] = useState(''); // Default query
+    const [currentQuery, setCurrentQuery] = useState(query); // For tracking the current query being searched
 
-    // useEffect(() => {
-    //     dispatch(fetchLatest(pagenum)); // Dispatch fetchLatest with page number
-    // }, [pagenum, dispatch]);
+    // Fetch news when the page number changes (but only for the current query)
+    useEffect(() => {
+        if (currentQuery) {
+            dispatch(fetchLatest({ pagenum, query: currentQuery }));
+        }
+    }, [pagenum, currentQuery, dispatch]);
 
+    // Fetch news for the updated query on Search button click
+    const fetchNews = () => {
+        setPagenum(1); // Reset to page 1
+        setCurrentQuery(query); // Trigger a search for the new query
+    };
+
+    // Pagination handlers
     const handlePrevPage = () => {
         if (pagenum > 1) {
-            setpagenum(pagenum - 1); // Decrease page number
+            setPagenum(pagenum - 1);
         }
     };
 
     const handleNextPage = () => {
-        setpagenum(pagenum + 1); // Increase page number
+        setPagenum(pagenum + 1);
     };
 
     return (
-        <div>
-            <ul>
-                {latestNews.map((news, index) => (
-                    <li key={index}>{news.author || 'Unknown Author'}</li>
-                ))}
-            </ul>
-            
-            <div className='flex gap-3 items-center'>
-                <button className='latbutt' onClick={handlePrevPage} disabled={pagenum <= 1}>Previous</button>
-                {pagenum}
-                <button className='latbutt' onClick={handleNextPage}>Next</button>
+        <div className="flex flex-col items-center justify-center">
+            <div className="w-full flex gap-3">
+                <input
+                    className="w-[70%] px-4 py-2 bg-stone-200 rounded-xl text-black"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Search a Topic"
+                />
+                <button
+                    onClick={fetchNews}
+                    className="w-[30%] px-4 py-2 bg-stone-200 rounded-xl"
+                >
+                    Search
+                </button>
             </div>
+
+            <ul>
+                {latestNews &&
+                    latestNews.map((news, index) => (
+                        <li key={index}>{news.author || 'Unknown Author'}</li>
+                    ))}
+            </ul>
+
+            {latestNews.length !=0 && (
+                <div className="flex gap-3 items-center">
+                    <button
+                        className="latbutt"
+                        onClick={handlePrevPage}
+                        disabled={pagenum <= 1}
+                    >
+                        Previous
+                    </button>
+                    {pagenum}
+                    <button className="latbutt" onClick={handleNextPage}>
+                        Next
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
--- src/store/latestSlice.jsx
+++ src/store/latestSlice.jsx
@@ -4,18 +4,23 @@ import axios from "axios";
 const api_key = import.meta.env.VITE_API_KEY;
 const url = 'https://newsapi.org/v2/everything';
 
-export const fetchLatest = createAsyncThunk('latest/fetchLatest', async (pagenum) => {
-    const response = await axios.get(url, {
-        params: {
-            apiKey: api_key,
-            sortBy: 'publishedAt',
-            pageSize: 10,
-            page:pagenum
-        }
-    })
 
-    return response.data.articles || []
-})
+export const fetchLatest = createAsyncThunk('latest/fetchLatest', async ({ pagenum, query }) => {
+    try {
+        const response = await axios.get(url, {
+            params: {
+                apiKey: api_key,
+                sortBy: 'publishedAt',
+                q: query,
+                pageSize: 10,
+                page: pagenum,
+            },
+        });
+        return response.data.articles || [];
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 const latestSlice = createSlice({
     name: "latest",
